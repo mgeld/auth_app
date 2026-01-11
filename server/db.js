6df30130049225498bff2c8aa9fbd79272e5e829
ANSAR <ansar@mail.ru>
2026-01-11 16:40:04 +0300

    dsksk

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
 import { Pool } from 'pg'
+import dotenv from 'dotenv';
+dotenv.config()
 
 const pool = new Pool({
-    user: 'postgres',
-    password: '12345',
-    host: 'localhost',
-    database: 'auth_app',
-    port: 5432
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_BASE,
+    port: Number(process.env.DB_PORT)
 })
 
 async function createUsers() {
